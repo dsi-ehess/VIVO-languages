8ba0fe7c3352ca3f4b208b71d6c7fd20ec246a67
michel heon <heon@ubuntu>
2020-12-02 06:11:01 -0500

    VIVO-languages_fr_FR: Creating directory structure for fr_FR

--- fr/home/src/main/resources/move.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Script pour changer les extension fr_FR en fr des fichiers contenu dans un repo
-# à utiliser avec 
-# find -name '*fr*' -type f -print -exec ./move.bash {} \;
-#
-echo mv $1 `echo $1 | sed "s/\_fr\_CA/\_fr/g"`
-mv $1 `echo $1 | sed "s/\_fr\_CA/\_fr/g"`
-
